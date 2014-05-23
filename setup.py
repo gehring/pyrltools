a09394996c33f5a6ae5437b8e4cb9fba6a2609a3
Clement Gehring <clement.gehring@gmail.com>
2014-05-23 13:55:02 -0400

    fixed setup.py, it should now be pip installable

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from numpy.distutils.core import setup, Extension
+#from numpy.distutils.core import setup, Extension
+from setuptools import setup, Extension, find_packages
 import numpy
 
 setup(name = 'rltools',
       version = '0.1',
       include_dirs=[numpy.get_include(), '/usr/include'],
+      packages = find_packages(),
       ext_modules= [Extension('rltools.ext_neuro',
                               ['c-src/neurosftd_libmod.c',
                                'c-src/neurosftd_lib.h'],
