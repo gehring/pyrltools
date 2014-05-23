0f919e155273b7e1810c0cd6fd827fbbf95df653
Clement Gehring <clement.gehring@gmail.com>
2014-05-23 14:04:06 -0400

    small tweak in the include directories in an attempt to solve an starcluster error

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ import numpy
 
 setup(name = 'rltools',
       version = '0.1',
-      include_dirs=[numpy.get_include(), '/usr/include'],
+      include_dirs=[numpy.get_include(), '/usr/include', 'c-src'],
       packages = find_packages(),
       ext_modules= [Extension('rltools.ext_neuro',
-                              ['c-src/neurosftd_libmod.c',
-                               'c-src/neurosftd_lib.h'],
+                              ['c-src/neurosftd_libmod.c'],
                               libraries = ['cblas'])])
