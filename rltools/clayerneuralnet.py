df177ba72291685b6f95244096a14579eadbb1ef
Clement Gehring <clement.gehring@gmail.com>
2014-05-28 11:17:46 -0400

    changed weight initialization

--- rltools/clayerneuralnet.py
+++ rltools/clayerneuralnet.py
@@ -176,9 +176,11 @@ class NeuronLayer(object):
 
         mommentum = argk.get('mommentum', 0.9)
         size_w = num_neuron * layer_input
-        init_w = np.random.uniform(-0.1, 0.1, size_w)
+#         init_w = np.random.uniform(-0.1, 0.1, size_w)
+        init_w = np.random.normal(0, 0.01, size_w)
         size_b = num_neuron * layer_input
-        init_b = np.random.uniform(-0.5, 0.5, size_b)
+#         init_b = np.random.uniform(-0.5, 0.5, size_b)
+        init_b = np.random.normal(0, 0.1, size_b)
 
 
         self.sigmoid = sigmoid
