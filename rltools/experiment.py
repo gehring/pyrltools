cb109c6e25d9b01cf9dc9d5d7a35ceb332f4639d
Clement Gehring <clement.gehring@gmail.com>
2014-05-30 15:01:11 -0400

    refactored experiment tools

--- rltools/experiment.py
+++ rltools/experiment.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import product, izip, repeat, imap, chain
 
 def evaluate_trial(domain, agent):
     domain_copy = domain.copy()
@@ -27,11 +28,11 @@ def evaluate_1000_steps(domain, agent):
 def evaluateAgent(domain, agent, evaluator, num_trials):
     return [ evaluator(domain, agent) for i in range(num_trials)]
 
-def train_steps_agent(domain, agent, evaluator, num_steps, num_eval, eval_interval):
+def train_steps_agent(domain, agent, evaluator, num_train_steps, num_eval, eval_interval):
     score= []
     r, s_t = domain.reset()
     agent.reset()
-    for i in xrange(num_steps):
+    for i in xrange(num_train_steps):
         if i % eval_interval == 0:
             score.append( np.mean(evaluateAgent(domain, agent, evaluator, num_eval)))
 
@@ -43,9 +44,9 @@ def train_steps_agent(domain, agent, evaluator, num_steps, num_eval, eval_interv
             r, s_t = domain.step(agent.step(r, s_t))
     return score
 
-def train_trials_agent(domain, agent, evaluator, num_trials, num_eval, eval_interval):
+def train_trials_agent(domain, agent, evaluator, num_train_steps, num_eval, eval_interval):
     score= []
-    for i in xrange(num_trials):
+    for i in xrange(num_train_steps):
         if i % eval_interval == 0:
                 score.append( np.mean(evaluateAgent(domain, agent, evaluator, num_eval)))
         r, s_t = domain.reset()
@@ -54,45 +55,32 @@ def train_trials_agent(domain, agent, evaluator, num_trials, num_eval, eval_inte
             r, s_t = domain.step(agent.step(r, s_t))
         agent.step(r,s_t)
 
+    return score
 
-
-def getRuns(**args):
-    alpha = args.get('alpha')
-    eta = args.get('eta')
-    epsilon = args.get('epsilon')
-    gamma = args.get('gamma')
-    mommentum = args.get('mommentum')
-    num_runs = args.get('num_runs', 10)
-
+def train_agent(**args):
     domain_factory = args.get('domain_factory')
     projector_factory = args.get('projector_factory')
     policy_factory = args.get('policy_factory')
     value_fn_factory = args.get('valuefn_factory')
     agent_factory = args.get('agent_factory')
-
-    param = {'alpha':alpha,
-             'eta':eta,
-             'epsilon':epsilon,
-             'gamma':gamma,
-             'mommentum':mommentum}
-
-    for i in range(num_runs):
-        domain = domain_factory(**args)
-        projector = projector_factory(domain = domain, **args)
-        param['layers'] = [projector.size] + args.get('layers', [40]) + [1]
-        valuefn = value_fn_factory(projector = projector, **param)
-        policy = policy_factory(domain = domain, valuefn = valuefn, **args)
-        agent = agent_factory(policy = policy, valuefn = valuefn, **args)
-
-        yield domain, agent
-
-def get_score_list(**args):
-    num_train_steps = args.get('num_train')
-    num_eval_trial = args.get('num_eval_trial')
-    eval_interval = args.get('eval_interval')
-    evaluator = args.get('evaluator', evaluate_trial)
     agent_trainer = args.get('trainer', train_steps_agent)
-    return map(lambda (d,a): agent_trainer(d, a, evaluator, num_train_steps,
-                                                    num_eval_trial,
-                                                    eval_interval),
-                            getRuns(**args))
+    key_parameter = args.get('key_parameters')
+
+    domain = domain_factory(**args)
+    projector = projector_factory(domain = domain, **args)
+    param = dict(args)
+    param['layers'] = [projector.size] + args.get('internal_layers', [40]) + [1]
+    valuefn = value_fn_factory(projector = projector, **param)
+    policy = policy_factory(domain = domain, valuefn = valuefn, **args)
+    agent = agent_factory(policy = policy, valuefn = valuefn, **args)
+
+    arguments = dict(args)
+    arguments['domain'] = domain
+    arguments['agent'] = agent
+    return tuple([ args[k] for k in key_parameter]), agent_trainer(**arguments)
+
+def getAllRuns(product_param):
+    # returns the product of every parameter as dict then generates every independent run
+    num_runs = product_param['num_runs']
+    return chain(*repeat(imap(dict,product(*(izip(repeat(k),v) for k,v in product_param.iteritems()))), num_runs))
+
