d912cf7f4fae9f1e41f7402645c5352f95b2bd4a
Clement Gehring <clement.gehring@gmail.com>
2014-08-08 12:38:25 -0400

    added avg rew neurosftd

--- rltools/valuefn.py
+++ rltools/valuefn.py
@@ -239,6 +239,58 @@ class TabularNeuroSFTD(ValueFn):
 
 
 
+class TabularAvgRewNeuroSFTD(ValueFn):
+    def __init__(self, actions, projector, alphamu, **argk):
+        super(TabularNeuroSFTD, self).__init__()
+        self.projector = projector
+        self.alphamu = alphamu
+        if 'layers' not in argk:
+            argk['layers'] = [projector.size, 30, 1]
+
+        self.actions = actions
+        self.nets = [NeuralNet( **argk) for i in xrange(len(actions))]
+        self.mu=None
+
+    def __call__(self, state, action):
+        projected = self.projector(state)
+        index = self.getactionindex(action)
+        return self.nets[index].evaluate(projected)[0]
+
+    def getactionindex(self, action):
+        return np.argmax(np.all(self.actions == action, axis = 1))
+
+    def update(self, s_t, a_t, r, s_tp1, a_tp1):
+        if self.mu == None:
+            self.mu = r
+        else:
+            self.mu = self.mu*(1-self.alphamu) + self.alphamu*r
+
+        if s_t == None:
+            return
+
+        index_t = self.getactionindex(a_t)
+        phi_t = self.projector(s_t)
+
+        if s_tp1 == None:
+            dphi = np.zeros_like(phi_t)
+            v_tp1 = 0
+        else:
+            phi_tp1 = self.projector(s_tp1)
+            index_tp1 = self.getactionindex(a_tp1)
+            v_tp1 = self.nets[index_tp1].evaluate(phi_tp1)[0]
+            dphi = phi_tp1 - phi_t
+
+        v_t = self.nets[index_t].evaluate(phi_t)[0]
+
+        dV = - r + self.mu
+
+        target = r - self.mu + v_tp1
+
+        self.nets[index_t].backprop(target, dphi, dV)
+
+
+
+
 class NeuroSFTD_Factory(object):
     def __init__(self, **argk):
         self.params = argk
