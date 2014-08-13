fixed bug in logisitic function 2nd derivative

rltools/pyneuralnet.py
@@ -15,7 +15,7 @@ class Logisticfn(object):
         return fx * (1-fx)
 
     def evaluatederivderiv(self, x, out=None):
-        emx = np.exp(-x)
+        emx = np.exp(x)
         return emx * (emx-1)/ (emx+1)**3
 
 class Linearfn(object):