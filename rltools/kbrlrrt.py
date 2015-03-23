217c95bf018b2c25a93f324a8aa77782da97a8e2
clement <clement.gehring@gmail.com>
2015-03-23 13:34:42 -0400

    fixed typo in rbf kernel

--- rltools/kbrlrrt.py
+++ rltools/kbrlrrt.py
@@ -121,7 +121,10 @@ class KBRLRRT(object):
         K = self.psi(samples[2], samples[0])
         
         # compute mass with bias
-        mass = np.sum(K, axis=1)
+        if K.ndim > 1:
+            mass = np.sum(K, axis=1)
+        else:
+            mass = np.sum(K)
         print samples
         print mass
         print K
